be9ae58958d73038364b7c3f530c1f2b3f7882f3
Peter Banka <peter.banka@cyaninc.com>
2016-02-08 20:29:30 -0800

    successful login!

--- actions/tradewindsActions.js
+++ actions/tradewindsActions.js
@@ -3,6 +3,7 @@
 import {AsyncStorage} from 'react-native';
 import * as types from './actionTypes';
 import _ from 'lodash';
+const CookieManager = require('react-native-cookies');
 
 const userDataKeys = [
     'username','password', 'cookie',
@@ -12,19 +13,19 @@ const userDataKeys = [
  *                        Synchronous actions                        *
  *********************************************************************/
 
-export function setBusy() {
+export function setBusy() : any {
   return {
     type: types.SET_BUSY
   };
 }
 
-export function clearBusy() {
+export function clearBusy(): any {
   return {
     type: types.CLEAR_BUSY
   };
 }
 
-export function setUser(savedUserData) {
+export function setUser(savedUserData: any): any {
   return {
     type: types.SET_USER_DATA,
     savedUserData,
@@ -66,3 +67,70 @@ export function readUserFromStorage() : any {
     }
 };
 
+function serializeJson (data) {
+  return Object.keys(data).map(function (keyName) {
+    return encodeURIComponent(keyName) + '=' + encodeURIComponent(data[keyName])
+  }).join('&');
+}
+
+function getExpiry() {
+  let exDate = new Date();
+  exDate.setDate(exDate.getDate() + 10);
+  return `${exDate.getUTCFullYear()}-${exDate.getUTCMonth()}-${exDate.getUTCDate()}T12:30:00.00-05:00`;
+}
+
+export function loginUser(userData: any) : any {
+  return function(dispatch) {
+
+    userData.password = '4zBDkV1Agi';
+    userData.username = '8637900';
+    const params = {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: 'userid=8637900&pwd=4zBDkV1Agi&Submit=Submit',
+    };
+
+    console.log('user is logging in!!!', userData);
+
+    fetch('http://www.tradewindssailing.com/wsdl/Logon.php', params)
+      .then(output => {
+
+        // list cookies
+        CookieManager.getAll((cookies, status) => {
+          console.log(cookies);
+          console.log(status);
+
+          // set cookie
+
+          //const newCookie = Object.assign({version: '1', expiration: getExpiry(), origin: 'tradewindssailing.com'}, cookies.PHPSESSID);
+          const newCookie = {
+            name: 'PHPSESSID',
+            value: cookies.PHPSESSID.value,
+            domain: 'www.tradewindssailing.com',
+            origin: 'www.tradewindssailing.com',
+            path: '/',
+            version: '1',
+            expiration: '2016-05-30T12:30:00.00-05:00'
+          }
+
+          CookieManager.set(newCookie, (err, res) => {
+            console.log('cookie set!');
+            console.log(err);
+            console.log(res);
+
+            return fetch('http://www.tradewindssailing.com/wsdl/Logon-action.php', params)
+              .then(logonAction => {
+                return fetch('http://www.tradewindssailing.com/wsdl/Reservations.php')
+                  .then(reservations => {
+                    debugger;
+                    console.log(reservations);
+                  });
+              });
+          });
+        });
+      });
+
+  }
+};
